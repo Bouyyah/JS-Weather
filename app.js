167b80cfdfb5f064846f9d0c880c33e981915923
bouyyah <hamameayyoub@gmail.com>
2020-09-24 04:11:38 +0100

    showing data

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-//events
+
 window.addEventListener('load', ()=>{
+    
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let weatherIcon = document.querySelector('.weather-icon');
+    
     let long;
     let lat;
 
@@ -8,7 +14,7 @@ window.addEventListener('load', ()=>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=2ed11c3acc76352a0ab2e352108a89e9`;
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=2ed11c3acc76352a0ab2e352108a89e9`;
             
                     
             fetch(api)
@@ -16,7 +22,12 @@ window.addEventListener('load', ()=>{
                 return response.json();
             })
             .then(data=>{
-                console.log(data);
+                console.log(data, data.main.temp);
+
+                temperatureDegree.textContent = data.main.temp;
+                temperatureDescription.textContent = data.weather[0].description;
+                locationTimezone.textContent = data.name;
+                weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
             })
         });
     }
